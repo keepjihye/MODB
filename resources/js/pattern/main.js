80daf043c6bc467015d61cde84bfe198df6f9f69
keepjihye <yoojihea60@gmail.com>
2024-11-08 14:00:17 +0900

    setting / header 구현

---
+++ resources/js/pattern/main.js
@@ -0,0 +1,31 @@
+
+// .all_menu클릭하면 #nav_all에 active 추가하기
+$('.all_menu').click(function () {
+    $('#nav-all').addClass("active");
+    $('.nav-all__wrap').addClass("active");
+});
+
+$('#nav-all').click(function () {
+    $('#nav-all').removeClass('active')
+    $('.nav-all__wrap').removeClass("active");;
+});
+
+
+
+// To - top / nav가리기
+window.addEventListener(
+    "scroll",
+    // thirottle 과부하 줄이기
+    _.throttle(function () {
+        const scrolled = window.scrollY;
+        if (scrolled > 80) {
+            gsap.to("#header", { yPercent: -100 });
+
+        } else {
+            gsap.to("#header", { yPercent: 0 });
+
+        }
+    })
+);
+
+
